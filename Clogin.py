f591f802463e49b323be09ce9d6f8271d1e7c714
equipa6 <equip_a6@boscdelacoma.cat>
2022-05-06 16:43:43 +0200

    versio 1.4

--- Clogin.py
+++ Clogin.py
@@ -1,50 +1,51 @@
 from tkinter import *
 import socket
 import threading
+from tkinter.font import BOLD
+from turtle import width
 
 def registredesessio():
     global ventana
     root.destroy()
     ventana = Tk()
     ventana.geometry("400x600")
+    ventana.geometry("+475+50")
     ventana.resizable(0,0)
     ventana.title("Registra't")
-    ventana.config(bg="#ccdadd")
+    ventana.config(bg="#FFFFFF")
 
     logoclogin = PhotoImage(file="logo.png")
     zoom_logo = logoclogin.subsample(7)
-    photo = Label(ventana, image=zoom_logo, bg="#ccdadd")
+    photo = Label(ventana, image=zoom_logo, bg="#FFFFFF")
     photo.place(x=125, y=0)
 
-    nomusuari = Label(ventana, text="Nom d'usuari", font=("Calibri", 18), bg="#ccdadd")
-    nomusuari.place(x=128, y=155)
+    nomusuari = Label(ventana, text="Nom d'usuari", font=("THIN", 16), bg="#FFFFFF")
+    nomusuari.place(x=135, y=170)
 
-    password = Label(ventana, text="Contrasenya", font=("Calibri", 18), bg="#ccdadd")
-    password.place(x=130, y=250)
+    password = Label(ventana, text="Contrasenya", font=("THIN", 16), bg="#FFFFFF")
+    password.place(x=135, y=260)
 
-    inpnom = Entry(ventana, font=("Calibri", 18), width=28)
+    inpnom = Entry(ventana, font=("Calibri", 18), width=28, borderwidth=1, relief="solid", bg="#ffee04")
     inpnom.place(x=28, y=205)
 
-    inpcontrasenya = Entry(ventana, font=("Calibri", 18), width=28)
+    inpcontrasenya = Entry(ventana, font=("Calibri", 18), width=28, borderwidth=1, relief="solid", bg="#ffee04")
     inpcontrasenya.place(x=28, y=295)
     inpcontrasenya.config(show="*")
 
-    confirmaciopassword = Label(ventana, text="Repeteix la contrasenya", font=("Calibri", 18), bg="#ccdadd")
-    confirmaciopassword.place(x=85, y=345)
+    confirmaciopassword = Label(ventana, text="Repeteix la contrasenya", font=("THIN", 16), bg="#FFFFFF")
+    confirmaciopassword.place(x=80, y=360)
 
-    inpconfirmaciocontrasenya = Entry(ventana, font=("Calibri", 18), width=28)
+    inpconfirmaciocontrasenya = Entry(ventana, font=("Calibri", 18), width=28, borderwidth=1, relief="solid", bg="#ffee04")
     inpconfirmaciocontrasenya.place(x=28, y=395)
     inpconfirmaciocontrasenya.config(show="*")
 
-    imagensingup = PhotoImage(file="register.png")
-    imgagensingup = imagensingup.subsample(2)
-    botonsingup = Button(ventana, image=imgagensingup, borderwidth=0, bg="#ccdadd", cursor="hand2")
-    botonsingup.place(x=95, y=475)
+    botonsingup = Button(ventana, text="Registra't", fg="#ffee04", bg="#606fff", cursor="hand2", font=("Calibri", 15, "bold"), width=18, borderwidth=0)
+    botonsingup.place(x=108, y=475)
 
     imatgeenrere = PhotoImage(file="enrere.png")
     imatgeenrere = imatgeenrere.subsample(10)
 
-    enrere = Button(ventana, command=inicidesessio, image = imatgeenrere, borderwidth=0, bg="#ccdadd", cursor="hand2")
+    enrere = Button(ventana, command=inicidesessio, image = imatgeenrere, borderwidth=0, bg="#FFFFFF", cursor="hand2")
     enrere.place(x=12, y=20)
 
     ventana.mainloop()
@@ -57,40 +58,42 @@ def inicidesessio():
         pass
     root = Tk()
     root.geometry("400x600")
+    root.geometry("+475+50")
     root.resizable(0,0)
-    root.title("Inicia sessió")
-    root.config(bg="#ccdadd")
+    root.title("Iniciar sessió")
+    root.config(bg="#FFFFFF")
 
     logo_clogin = PhotoImage(file="logo.png")
     zoom = logo_clogin.subsample(7)
-    foto = Label(root, image=zoom, bg="#ccdadd")
+    foto = Label(root, image=zoom, bg="#FFFFFF")
     foto.place(x=125, y=0)
 
-    nom_usuari = Label(root, text="Nom d'usuari", font=("Calibri", 18), bg="#ccdadd")
-    nom_usuari.place(x=128, y=150)
+    nom_usuari = Label(root, text="Nom d'usuari", font=("THIN", 16), bg="#FFFFFF")
+    nom_usuari.place(x=135, y=165)
 
-    contrasenya = Label(root, text="Contrasenya", font=("Calibri", 18), bg="#ccdadd")
-    contrasenya.place(x=130, y=260)
+    contrasenya = Label(root, text="Contrasenya", font=("THIN", 16), bg="#FFFFFF")
+    contrasenya.place(x=135, y=265)
 
-    inp_nom = Entry(root, font=("Calibri", 18), width=28)
+    inp_nom = Entry(root, font=("Calibri", 18), width=28, borderwidth=1, relief="solid", bg="#ffee04")
     inp_nom.place(x=28, y=200)
 
-    inp_contrasenya = Entry(root, font=("Calibri", 18), width=28)
+    inp_contrasenya = Entry(root, font=("Calibri", 18), width=28, borderwidth=1, relief="solid", bg="#ffee04")
     inp_contrasenya.place(x=28, y=300)
     inp_contrasenya.config(show="*")
 
-    imagen_singup = PhotoImage(file="singup.png")
-    imgagen_singup = imagen_singup.subsample(5)
-    boton_singup = Button(root, image=imgagen_singup, borderwidth=0, bg="#ccdadd", cursor="hand2")
-    boton_singup.place(x=125, y=370)
+    boton_singup = Button(root, text="Entra", fg="#ffee04", bg="#606fff", cursor="hand2", font=("Calibri", 15, "bold"), width=18, borderwidth=0)
+    boton_singup.place(x=105, y=370)
 
-    lab_register = Label(root, text="No tens compte", font=("Calibri", 14, "underline"), bg="#ccdadd")
-    lab_register.place(x=50, y=530)
+    lab_register = Label(root, text="No tens compte?", font=("THIN", 14, "underline"), bg="#FFFFFF")
+    lab_register.place(x=35, y=530)
 
     foto_register = PhotoImage(file="register.png")
     foto_register = foto_register.subsample(2)
-    button_register = Button(root, image=foto_register, bg="#ccdadd",borderwidth=0, command=registredesessio)
-    button_register.place(x=175, y=510)
+
+    button_register = Button(root, text="Registra't", fg="#ffee04", bg="#606fff", cursor="hand2", font=("Calibri", 15, "bold"), width=16, borderwidth=0, command=registredesessio)
+    button_register.place(x=195, y=525)
+    
+    
 
     root.mainloop()
 
